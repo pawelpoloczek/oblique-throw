007edd0851beda87d72b4bc2c776a945d688e853
Paweł Poloczek <pawel@poloczek.me>
2022-01-09 20:01:58 +0100

    Add entry data form

---
+++ src/Form/Type/EntryDataType.php
@@ -0,0 +1,32 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\AtLeastOneOf;
+use Symfony\Component\Validator\Constraints\Blank;
+use Symfony\Component\Validator\Constraints\PositiveOrZero;
+
+class EntryDataType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('initialSpeed', NumberType::class, [
+                'required' => true,
+                'constraints' => [new PositiveOrZero()],
+            ])
+            ->add('throwAngle', NumberType::class, [
+                'required' => true,
+                'constraints' => [new PositiveOrZero()],
+            ])
+            ->add('time', NumberType::class, [
+                'required' => true,
+                'constraints' => [new AtLeastOneOf([new Blank(), new PositiveOrZero()])],
+            ])
+        ;
+    }
+}
