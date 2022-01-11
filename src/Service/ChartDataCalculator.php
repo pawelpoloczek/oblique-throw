7d147e5b1370d429e3c19321269cde35070504cf
Paweł Poloczek <pawel@poloczek.me>
2022-01-11 01:09:01 +0100

    Chart data calculator

--- src/Service/ChartDataCalculator.php
+++ src/Service/ChartDataCalculator.php
@@ -26,6 +26,12 @@ class ChartDataCalculator
         $totalTime = (2 * $chartData->getInitialSpeed() * $sinAlpha) / self::G;
         $chartData->setTotalTime($totalTime);
 
+        $range = (2 * sqrt($chartData->getInitialSpeed()) * $sinAlpha * $cosAlpha) / self::G;
+        $chartData->setRange($range);
+
+        $maxHeight = sqrt($chartData->getInitialSpeedVertical()) / 2 * self::G;
+        $chartData->setMaximumHeight($maxHeight);
+
         return $chartData;
     }
 }
