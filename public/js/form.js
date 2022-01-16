e7c059d71e9d639ba0d746e5c134e0e94e9f5d4d
Paweł Poloczek <pawel@poloczek.me>
2022-01-16 02:29:36 +0100

    Display calculated data on front

--- public/js/form.js
+++ public/js/form.js
@@ -16,6 +16,17 @@ $(document).ready(function() {
                     $.each(response.errors, function (index, error){
                         $('#errors').append('<div class="error">' + index + ': ' + error + '</div>');
                     });
+                } else {
+                    $('#data').empty();
+                    $('#data').append(
+                        '<div>Prędkość początkowa: ' + response.data.initialSpeed + ' m/s</div>'
+                        + '<div>Składowa Y prędkości początkowej: ' + response.data.initialSpeedVertical + ' m/s</div>'
+                        + '<div>Składowa X prędkości początkowej: ' + response.data.initialSpeedHorizontal + ' m/s</div>'
+                        + '<div>Kąt rzutu: ' + response.data.throwAngle + '</div>'
+                        + '<div>Maksymalna wysokość: ' + response.data.maximumHeight + ' m</div>'
+                        + '<div>Zasięg: ' + response.data.range + ' m</div>'
+                        + '<div>Całkowity czas: ' + response.data.totalTime + ' s</div>'
+                    );
                 }
                 console.log(response);
             }
